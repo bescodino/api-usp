25c7f1020486a0801fa8b6f6bb1f958694008273
Bruno Escodino <bescodino@gmail.com>
2024-02-22 11:08:44 -0300

    Update UserDTO.cs

--- LabSid.Api/DTO/UserDTO.cs
+++ LabSid.Api/DTO/UserDTO.cs
@@ -4,7 +4,7 @@ namespace LabSid.Api.DTO
 {
     public class UserDTO
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
